e54a1707865f027a693dfa1703ec765b42135500
very10V3H <2016187250@qq.com>
2024-07-26 22:10:50 +0800

    [bug修复] 修复活动物品绑定不生效的bug

--- src/main/java/com/very/wraq/series/specialevents/summer/SummerEvent.java
+++ src/main/java/com/very/wraq/series/specialevents/summer/SummerEvent.java
@@ -43,15 +43,16 @@ public class SummerEvent {
             incrementIntData(player, dailySwimmingSecondsKey);
             incrementIntData(player, totalSwimmingSecondsKey);
             int dailySwimmingSeconds = getIntData(player, dailySwimmingSecondsKey);
-            if (dailySwimmingSeconds % 300 == 1) sendFormatMSG(player, Component.literal("炎热的天气，在水中").withStyle(ChatFormatting.WHITE).
-                    append(Component.literal("享受清凉").withStyle(CustomStyle.styleOfWater)).
-                    append(Component.literal("吧！").withStyle(ChatFormatting.WHITE)).
-                    append(Component.literal("(游泳速度已+200%)").withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC)));
+            if (dailySwimmingSeconds % 300 == 1)
+                sendFormatMSG(player, Component.literal("炎热的天气，在水中").withStyle(ChatFormatting.WHITE).
+                        append(Component.literal("享受清凉").withStyle(CustomStyle.styleOfWater)).
+                        append(Component.literal("吧！").withStyle(ChatFormatting.WHITE)).
+                        append(Component.literal("(游泳速度已+200%)").withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC)));
             if (dailySwimmingSeconds % 15 >= 3) {
                 if (dailySwimmingSeconds % 3 == 0) Compute.soundToPlayer(player, SoundEvents.FISH_SWIM);
                 Compute.sendActionBarMSG(player, Component.literal("正在摸鱼" + ".".repeat((dailySwimmingSeconds % 15) / 3)).withStyle(CustomStyle.styleOfWater));
             }
-            reward(player);
+            if (dailySwimmingSeconds % 15 == 0) reward(player);
             if (dailySwimmingSeconds % (15 * 60) == 0) {
                 playerExHarvestEndTick.put(name, tick + (15 * 60 * 20));
                 sendFormatMSG(player, Component.literal("因为").withStyle(ChatFormatting.WHITE).
@@ -118,73 +119,73 @@ public class SummerEvent {
 
     public static void reward(Player player) throws SQLException {
         int dailySwimmingSeconds = getIntData(player, dailySwimmingSecondsKey);
-        if (dailySwimmingSeconds % 15 == 0) {
-            Compute.soundToPlayer(player, SoundEvents.EXPERIENCE_ORB_PICKUP);
-            Component timeFormat;
-            if (dailySwimmingSeconds >= 600) {
-                timeFormat = Component.literal(String.format("%.2f", dailySwimmingSeconds / 60.0) + "min").withStyle(CustomStyle.styleOfWater);
-                if (dailySwimmingSeconds % 600 == 0) {
-                    Compute.formatBroad(player.level(), Component.literal("暑期活动").withStyle(CustomStyle.styleOfPower),
-                            Component.literal("").withStyle(ChatFormatting.WHITE).
-                                    append(player.getDisplayName()).
-                                    append(Component.literal(" 今日").withStyle(ChatFormatting.WHITE)).
-                                    append(Component.literal("摸鱼时间").withStyle(CustomStyle.styleOfWater)).
-                                    append(Component.literal("达到了: ").withStyle(ChatFormatting.WHITE)).
-                                    append(timeFormat));
-                }
-            } else {
-                timeFormat = Component.literal(dailySwimmingSeconds + "s").withStyle(CustomStyle.styleOfWater);
+        Compute.soundToPlayer(player, SoundEvents.EXPERIENCE_ORB_PICKUP);
+        Component timeFormat;
+        if (dailySwimmingSeconds >= 600) {
+            timeFormat = Component.literal(String.format("%.2f", dailySwimmingSeconds / 60.0) + "min").withStyle(CustomStyle.styleOfWater);
+            if (dailySwimmingSeconds % 600 == 0) {
+                Compute.formatBroad(player.level(), Component.literal("暑期活动").withStyle(CustomStyle.styleOfPower),
+                        Component.literal("").withStyle(ChatFormatting.WHITE).
+                                append(player.getDisplayName()).
+                                append(Component.literal(" 今日").withStyle(ChatFormatting.WHITE)).
+                                append(Component.literal("摸鱼时间").withStyle(CustomStyle.styleOfWater)).
+                                append(Component.literal("达到了: ").withStyle(ChatFormatting.WHITE)).
+                                append(timeFormat));
             }
-            Compute.sendActionBarMSG(player, Component.literal("今日摸鱼: ").withStyle(CustomStyle.styleOfWater).append(timeFormat));
-            Random random = new Random();
-            double index = random.nextDouble();
-            boolean quitJudge = false;
-            if (index < 0.01) {
-                int summerVoucherGetTimesKey = getIntData(player, dailySummerVoucherGetTimesKey);
-                if (summerVoucherGetTimesKey < 8) {
-                    quitJudge = true;
-                    incrementIntData(player, dailySummerVoucherGetTimesKey);
-                    Compute.itemStackGive(player, new ItemStack(SpecialEventItems.SUMMER_VOUCHER.get()));
-                    sendFormatMSG(player, Component.literal("今日还能获得").withStyle(ChatFormatting.WHITE).
-                            append(Component.literal(String.valueOf(8 - (summerVoucherGetTimesKey + 1)) + "次").withStyle(CustomStyle.styleOfWater)).
-                            append(SpecialEventItems.SUMMER_VOUCHER.get().getDefaultInstance().getDisplayName()));
-                } else index += 0.01;
-            }
+        } else {
+            timeFormat = Component.literal(dailySwimmingSeconds + "s").withStyle(CustomStyle.styleOfWater);
+        }
+        Compute.sendActionBarMSG(player, Component.literal("今日摸鱼: ").withStyle(CustomStyle.styleOfWater).append(timeFormat));
+        Random random = new Random();
+        double index = random.nextDouble();
+        boolean quitJudge = false;
+        if (index < 0.06) {
+            int summerVoucherGetTimesKey = getIntData(player, dailySummerVoucherGetTimesKey);
+            if (summerVoucherGetTimesKey < 8) {
+                quitJudge = true;
+                incrementIntData(player, dailySummerVoucherGetTimesKey);
+                Compute.itemStackGive(player, new ItemStack(SpecialEventItems.SUMMER_VOUCHER.get()));
+                sendFormatMSG(player, Component.literal("今日还能获得").withStyle(ChatFormatting.WHITE).
+                        append(Component.literal(String.valueOf(8 - (summerVoucherGetTimesKey + 1)) + "次").withStyle(CustomStyle.styleOfWater)).
+                        append(SpecialEventItems.SUMMER_VOUCHER.get().getDefaultInstance().getDisplayName()));
+            } else index += 0.06;
+        }
 
-            if (!quitJudge && index < 0.02) {
-                int dailyWorldSoul5GetTimes = getIntData(player, dailyWorldSoul5GetTimesKey);
-                if (dailyWorldSoul5GetTimes < 8) {
-                    quitJudge = true;
-                    incrementIntData(player, dailyWorldSoul5GetTimesKey);
-                    Tower.givePlayerStar(player, 5, "summer event");
-                    sendFormatMSG(player, Component.literal("今日还能获得").withStyle(ChatFormatting.WHITE).
-                            append(Component.literal(String.valueOf(8 - dailyWorldSoul5GetTimes + 1) + "次").withStyle(CustomStyle.styleOfWater)).
-                            append(ModItems.worldSoul5.get().getDefaultInstance().getDisplayName()));
-                } else index += 0.01;
-            }
+        if (!quitJudge && index < 0.12) {
+            int dailyWorldSoul5GetTimes = getIntData(player, dailyWorldSoul5GetTimesKey);
+            if (dailyWorldSoul5GetTimes < 8) {
+                quitJudge = true;
+                incrementIntData(player, dailyWorldSoul5GetTimesKey);
+                Tower.givePlayerStar(player, 5, "summer event");
+                sendFormatMSG(player, Component.literal("今日还能获得").withStyle(ChatFormatting.WHITE).
+                        append(Component.literal(String.valueOf(8 - dailyWorldSoul5GetTimes + 1) + "次").withStyle(CustomStyle.styleOfWater)).
+                        append(ModItems.worldSoul5.get().getDefaultInstance().getDisplayName()));
+            } else index += 0.08;
+        }
 
-            if (!quitJudge && index < 0.45) {
-                int dailyExpGetTimes = getIntData(player, dailyExpGetTimesKey);
-                if (dailyExpGetTimes < 20) {
-                    quitJudge = true;
-                    incrementIntData(player, dailyExpGetTimesKey);
-                    Compute.ExpPercentGetIgnoreLimitAndMSGSend(player, 0.05, 0, player.experienceLevel);
-                    sendFormatMSG(player, Component.literal("今日还能获得").withStyle(ChatFormatting.WHITE).
-                            append(Component.literal(String.valueOf(20 - (dailyExpGetTimes + 1)) + "次").withStyle(CustomStyle.styleOfWater)).
-                            append(Component.literal("经验").withStyle(ChatFormatting.LIGHT_PURPLE)));
-                }
-            }
-
-            if (!quitJudge) {
-                Compute.itemStackGive(player, new ItemStack(ModItems.SeaSoul.get(), 3));
+        if (!quitJudge && index < 0.2) {
+            int dailyExpGetTimes = getIntData(player, dailyExpGetTimesKey);
+            if (dailyExpGetTimes < 20) {
+                quitJudge = true;
+                incrementIntData(player, dailyExpGetTimesKey);
+                Compute.ExpPercentGetIgnoreLimitAndMSGSend(player, 0.05, 0, player.experienceLevel);
+                sendFormatMSG(player, Component.literal("今日还能获得").withStyle(ChatFormatting.WHITE).
+                        append(Component.literal(String.valueOf(20 - (dailyExpGetTimes + 1)) + "次").withStyle(CustomStyle.styleOfWater)).
+                        append(Component.literal("经验").withStyle(ChatFormatting.LIGHT_PURPLE)));
             }
         }
+
+        if (!quitJudge) {
+            Compute.itemStackGive(player, new ItemStack(ModItems.SeaSoul.get(), 3));
+        }
+
     }
 
     public static void sendDailyTimeRank(Level level) {
         Compute.formatBroad(level, Component.literal("暑期活动"), Component.literal("当前在线玩家的").withStyle(ChatFormatting.WHITE).
                 append(Component.literal("摸鱼时长排名 >>").withStyle(CustomStyle.styleOfWater)));
-        record PlayerTime(Player player, int seconds){}
+        record PlayerTime(Player player, int seconds) {
+        }
         List<PlayerTime> playerDailyTimeList = new ArrayList<>();
         level.getServer().getPlayerList().getPlayers().forEach(serverPlayer -> {
             playerDailyTimeList.add(new PlayerTime(serverPlayer, getIntData(serverPlayer, dailySwimmingSecondsKey)));
