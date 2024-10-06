fe6e83bcc5f0c17d7210548e0f4252f15270ce5e
very10V3H <2016187250@qq.com>
2024-10-06 17:47:43 +0800

    [代码改动] 获取离线玩家persistentData方法

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,24 @@
-import fun.wraq.common.attribute.MobAttributes;
-import fun.wraq.files.CSVReader;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(MobAttributes.readAttributes(CSVReader.readFile("C:\\Users\\Administrator\\Desktop\\mobAttributes.csv")));
+        File directory = new File("");
+        String path = directory.getAbsolutePath() + "\\run\\world\\playerdata\\0b2cab89-79a6-3138-9f56-4d366bfc99f2-7547368493861204727.dat";
+        File data = new File(path);
+        if (data.exists() && data.isFile()) {
+            CompoundTag tag = NbtIo.readCompressed(data);
+            System.out.println(tag.getCompound("ForgeData"));
+        }
+
+/*        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(new FileReader(path));
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+        System.out.println(jsonObject.asMap());*/
     }
 }
 
