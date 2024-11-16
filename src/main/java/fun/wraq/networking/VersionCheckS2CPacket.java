95cf52aaeadfad3bba487bdaef584edea5d2b7db
very10V3H <2016187250@qq.com>
2024-11-16 18:14:00 +0800

    [v2.0.38b] 6.森林粉碎者主动更改 8.合剂属性bug 9.城堡武器主动bug + buff

--- src/main/java/fun/wraq/networking/VersionCheckS2CPacket.java
+++ src/main/java/fun/wraq/networking/VersionCheckS2CPacket.java
@@ -22,7 +22,7 @@ public class VersionCheckS2CPacket {
     public boolean handle(Supplier<NetworkEvent.Context> supplier) {
         NetworkEvent.Context context = supplier.get();
         context.enqueueWork(() -> {
-            ModNetworking.sendToServer(new VersionC2SPacket("2.0.38"));
+            ModNetworking.sendToServer(new VersionC2SPacket("2.0.38b"));
         });
         return true;
     }
