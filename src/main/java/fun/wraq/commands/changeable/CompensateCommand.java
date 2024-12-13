51f3eecc1f9b626f8ff068330b5e0786b37f5a65
very10V3H <2016187250@qq.com>
2024-12-13 22:33:16 +0800

    [v2.0.41]

--- src/main/java/fun/wraq/commands/changeable/CompensateCommand.java
+++ src/main/java/fun/wraq/commands/changeable/CompensateCommand.java
@@ -18,7 +18,7 @@ import net.minecraft.world.item.ItemStack;
 public class CompensateCommand implements Command<CommandSourceStack> {
     public static CompensateCommand instance = new CompensateCommand();
 
-    public static int rewardNum = 18;
+    public static int rewardNum = 19;
     public static String singleReward = "singleReward" + rewardNum;
 
     @Override
